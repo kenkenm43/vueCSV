0f1d3470d3f8c6cce05f2974ecb8ccd10d32d826
kenkenm43 <kenkenm43@gmail.com>
2022-02-21 14:22:41 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="text-align:center;" >
+    <h1>Export JSON to Excel</h1>
+    <button @click="getReport" >Export</button> <!-- เพิ่มปุ่ม Export -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data(){
+    return {
+      // ข้อมูล JSON ทีต้องการ Export
+      json : [
+        { name: 'Dady', age: '21' },
+        { name: 'Jonh', age: '25' },
+        { name: 'James', age: '17' },
+      ]
+    }
+  },
+  methods: {
+    // เมื่อกดปุ่มจะทำการสร้างไฟล์ xcel ด้วย xlsx
+    objectToCsv(data) {
+      const csvRows = []
+      const headers = Object.keys(data[0])
+      csvRows.push(headers.join(','))
+
+      for(const row of data) {
+        const values = headers.map(header => {
+          const escaped = (''+row[header]).replace(/"/g, '\\"')
+          return `"${escaped}"`
+        })
+        csvRows.push(values.join(','))
+      }
+      return csvRows.join('\n')
+    },
+    download (data) {
+      const blob = new Blob([data],{type:'text/csv'})
+      const url = window.URL.createObjectURL(blob)
+      const a = document.createElement('a')
+      a.setAttribute('hidden','')
+      a.setAttribute('href',url)
+      a.setAttribute('download','download.csv')
+      document.body.appendChild(a)
+      a.click()
+      document.body.removeChild(a)
+    },
+    async getReport() {
+      const csvData = this.objectToCsv(this.json)
+      this.download(csvData)
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
